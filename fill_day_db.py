3cac2cbae5a4efbb6e32e44cd4272183ebda7e9b
dvpuchades <dapucto@upv.edu.es>
2021-07-30 19:14:45 +0200

    more products for days db

--- fill_day_db.py
+++ fill_day_db.py
@@ -35,69 +35,71 @@ def get_one_day_ago(str_day):
     else:
         return str(day - 1)+'-'+str(month)+'-'+str(year)
 
-day = '28-07-2021'
+day = '29-07-2021'
 
 data_list = []
 
-for i in range(2000):
-    data = cg.get_coin_history_by_id(id='bitcoin', date=day)
-    data_list.append([day, data])
-    day = get_one_day_ago(day)
-    if(len(data_list) == 91):
-        date = data_list[1][0]
-        product = 'bitcoin'
+product = ['bitcoin', 'ethereum', 'ripple', 'tether', 'binancecoin', 'polkadot', 'litecoin', 'stellar']
 
-        price_list = []
-        for j in range(5, 0, -1):
-            price_list.append(float(data_list[j][1]['market_data']['current_price']['usd']))
-        
-        market_cap =float(data_list[0][1]['market_data']['market_cap']['usd'])
+for p in product:
+    for i in range(2000):
+        data = cg.get_coin_history_by_id(id=p, date=day)
+        data_list.append([day, data])
+        day = get_one_day_ago(day)
+        if(len(data_list) == 91):
+            date = data_list[1][0]
 
-        average_15 = 0
-        for j in range(1, 16):
-            average_15 += float(data_list[j][1]['market_data']['current_price']['usd']) / 15
+            price_list = []
+            for j in range(5, 0, -1):
+                price_list.append(float(data_list[j][1]['market_data']['current_price']['usd']))
+            
+            market_cap =float(data_list[0][1]['market_data']['market_cap']['usd'])
 
-        average_30 = 0
-        for j in range(1, 31):
-            average_30 += float(data_list[j][1]['market_data']['current_price']['usd']) / 30
+            average_15 = 0
+            for j in range(1, 16):
+                average_15 += float(data_list[j][1]['market_data']['current_price']['usd']) / 15
 
-        average_90 = 0
-        for j in range(1, 91):
-            average_90 += float(data_list[j][1]['market_data']['current_price']['usd']) / 90
-        
-        result = float(data_list[0][1]['market_data']['current_price']['usd'])
+            average_30 = 0
+            for j in range(1, 31):
+                average_30 += float(data_list[j][1]['market_data']['current_price']['usd']) / 30
 
-        d = Day(date, product, price_list, market_cap, average_15, average_30, average_90)
+            average_90 = 0
+            for j in range(1, 91):
+                average_90 += float(data_list[j][1]['market_data']['current_price']['usd']) / 90
+            
+            result = float(data_list[0][1]['market_data']['current_price']['usd'])
 
-        d.twitter_followers = data_list[1][1]['community_data']['twitter_followers']
-        d.reddit_average_posts_48h = data_list[1][1]['community_data']['reddit_average_posts_48h']
-        d.reddit_average_comments_48h = data_list[1][1]['community_data']['reddit_average_comments_48h']
-        d.alexa_rank = data_list[1][1]['public_interest_stats']['alexa_rank']
+            d = Day(date, product, price_list, market_cap, average_15, average_30, average_90)
 
-        if d.twitter_followers == None:
-            d.twitter_followers = 0
-        else:
-            d.twitter_followers = int(d.twitter_followers)
+            d.twitter_followers = data_list[1][1]['community_data']['twitter_followers']
+            d.reddit_average_posts_48h = data_list[1][1]['community_data']['reddit_average_posts_48h']
+            d.reddit_average_comments_48h = data_list[1][1]['community_data']['reddit_average_comments_48h']
+            d.alexa_rank = data_list[1][1]['public_interest_stats']['alexa_rank']
 
-        if d.reddit_average_posts_48h == None:
-            d.reddit_average_posts_48h = 0
-        else:
-            d.reddit_average_posts_48h = float(d.reddit_average_posts_48h)
+            if d.twitter_followers == None:
+                d.twitter_followers = 0
+            else:
+                d.twitter_followers = int(d.twitter_followers)
 
-        if d.reddit_average_comments_48h == None:
-            d.reddit_average_comments_48h = 0
-        else:
-            d.reddit_average_comments_48h = float(d.reddit_average_comments_48h)
+            if d.reddit_average_posts_48h == None:
+                d.reddit_average_posts_48h = 0
+            else:
+                d.reddit_average_posts_48h = float(d.reddit_average_posts_48h)
 
-        if d.alexa_rank == None:
-            d.alexa_rank = 0
-        else:
-            d.alexa_rank = int(d.alexa_rank)
+            if d.reddit_average_comments_48h == None:
+                d.reddit_average_comments_48h = 0
+            else:
+                d.reddit_average_comments_48h = float(d.reddit_average_comments_48h)
 
-        d.set_result(result)
-        
-        collection.insert_one(d.to_dict()) #insertar en db
+            if d.alexa_rank == None:
+                d.alexa_rank = 0
+            else:
+                d.alexa_rank = int(d.alexa_rank)
 
-        data_list.pop(0)
+            d.set_result(result)
+            
+            collection.insert_one(d.to_dict()) #insertar en db
 
-    time.sleep(3)
+            data_list.pop(0)
+
+        time.sleep(3)
