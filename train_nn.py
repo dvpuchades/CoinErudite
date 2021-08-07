68839eeffc329a22e46c1531d1a2364fcbd90088
dvpuchades <dapucto@upv.edu.es>
2021-08-07 21:21:49 +0200

    UNSTABLE_FLAG beth version

--- train_nn.py
+++ train_nn.py
@@ -1,14 +1,15 @@
 from util import coach
+from util import beth_coach
 from nn import ted
 from nn import beth
 from nn import rick
 import torch
 
-nn = rick.Rick()
+nn = beth.Beth()
 # nn.load_state_dict(torch.load('ted_3.pkl'))
-c = coach.Coach(nn)
-c.set_iterations(0, 10000)
-c.epochs = 2
+c = beth_coach.Coach(nn)
+c.set_iterations(110000, 30000)
+c.epochs = 3
 c.set_collection('Minutes')
 c.train()
 # torch.save(t.state_dict(), 'ted_4.pkl')
